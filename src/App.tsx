f4769865a9a357edd4050fe5f5f2adac7737f321
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-05-25 18:38:04 +0100

    ADD: create pages folder and set pages inside

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login, Register } from "./components";
+import { Login, Register } from "./pages";
 
 export const App = () => {
 	return (
