824e71a391173f6c3e44949c99df3304139282e8
DavidMezadri <davidmezadri@gmail.com>
2025-05-28 22:25:00 -0300

    Ajustantando Gráficos

--- src/Pages/Table/Table.tsx
+++ src/Pages/Table/Table.tsx
@@ -1,12 +1,48 @@
 import "./styles.module.css";
-import { Charts } from "../../components/Charts/Charts";
+import { type ChartDataItem, Charts } from "../../components/Charts/Charts";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+
+const chartData: ChartDataItem[] = [
+	{ element: "pH", running: 450, swimming: 300 },
+	{ element: "CO", running: 380, swimming: 420 },
+	{ element: "MO", running: 520, swimming: 120 },
+	{ element: "CO", running: 140, swimming: 550 },
+	{ element: "P", running: 600, swimming: 350 },
+	{ element: "K", running: 480, swimming: 400 },
+	{ element: "Ca", running: 480, swimming: 500 },
+	{ element: "Mg", running: 480, swimming: 500 },
+	{ element: "S", running: 140, swimming: 550 },
+	{ element: "B", running: 600, swimming: 350 },
+	{ element: "Zn", running: 480, swimming: 400 },
+	{ element: "Cu", running: 480, swimming: 500 },
+	{ element: "Mn", running: 480, swimming: 500 },
+	{ element: "Fe", running: 480, swimming: 400 },
+	{ element: "Al", running: 480, swimming: 500 },
+	{ element: "CTC", running: 480, swimming: 500 },
+	{ element: "V%", running: 140, swimming: 550 },
+	{ element: "H+Al", running: 600, swimming: 350 },
+];
+
+const chartConfig = {
+	running: {
+		label: "Atual",
+		color: "hsl(var(--chart-1))",
+	},
+	swimming: {
+		label: "Ideal",
+		color: "hsl(var(--chart-2))",
+	},
+};
 
 export const Table = () => {
 	return (
-		<div className="bg-background text-center pt-[0vh] h-[100vh]">
-			<div className="flex items-center justify-center bg-opacity-50 ">
-				<div className="bg-surface-variant lg:w-[80vw] w-[100vw] h-[75vh] overflow-y-auto p-6 rounded-lg shadow-xl">
-					<Charts />
+		<div>
+			<Sidebar />
+			<div className="grid place-items-center h-screen bg-background">
+				<div className="lg:w-[70vw] w-[100vw] h-[95vh]">
+					<div className="rounded-lg shadow-xl">
+						<Charts chartConfig={chartConfig} chartData={chartData} />
+					</div>
 				</div>
 			</div>
 		</div>
